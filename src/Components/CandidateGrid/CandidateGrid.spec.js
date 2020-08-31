c439f3fd3de16334371ced61dc77ded4202a29df
cejiofor <cejiofor@gmail.com>
2020-08-31 16:07:58 -0500

    Remove CandidateGrid test

--- src/Components/CandidateGrid/CandidateGrid.spec.js
+++ src/Components/CandidateGrid/CandidateGrid.spec.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CandidateGrid from './CandidateGrid';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import CandidateGrid from './CandidateGrid';
+// import {BrowserRouter as Router, Route} from 'react-router-dom';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Router>
-    <Route path="/candidates" component={CandidateGrid}></Route>
-    </Router>, div);
-});
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<Router>
+//     <Route path="/candidates" component={CandidateGrid}></Route>
+//     </Router>, div);
+// });
