49af55866118ecf2406c680765596d4e8d540b63
cejiofor <cejiofor@gmail.com>
2020-08-31 15:47:54 -0500

    Add CandidateGrid test

--- src/utility/api.js
+++ src/utility/api.js
@@ -4,6 +4,6 @@ const realUrl = 'https://nsbedfw-job-board-backend.herokuapp.com';
 const dummyUrl = 'http://localhost:8080';
 
 export default axios.create({
-  baseURL: `${dummyUrl}`,
+  baseURL: `${realUrl}`,
   responseType: 'json',
 });
